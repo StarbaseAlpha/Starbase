bfc7cacceded77e2868ffe3a6111f26b675cd8d5
Mike Fuller <emailthemike@gmail.com>
2018-11-23 02:48:39 -0500

    added project files

---
+++ build-min/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+wget https://cdn.jsdelivr.net/npm/@starbase/database/database.min.js
+wget https://cdn.jsdelivr.net/npm/@starbase/database/memstore.min.js
+wget https://cdn.jsdelivr.net/npm/@starbase/channels/channels.min.js
+wget https://cdn.jsdelivr.net/npm/@starbase/channels/api/channels.api.min.js
+wget https://cdn.jsdelivr.net/npm/@starbase/socket/socket.min.js
+wget https://cdn.jsdelivr.net/npm/@starbase/therules/therules.min.js
+wget https://cdn.jsdelivr.net/npm/@starbase/encryption/encryption.min.js
+
+cat STARTjs database.min.js memstore.min.js channels.min.js channels.api.min.js socket.min.js therules.min.js encryption.min.js ENDjs > starbase.js
+
+uglifyjs -c -o ../starbase.min.js starbase.js
+rm starbase.js
+rm database.min.js
+rm memstore.min.js
+rm channels.min.js
+rm channels.api.min.js
+rm socket.min.js
+rm therules.min.js
+rm encryption.min.js
---
+++ node/starbase.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Defacto = require('@starbase/defacto');
+const Database = require('@starbase/database');
+const memstore = require('@starbase/database/memstore');
+const DBService = require('@starbase/database/server/dbservice');
+const Channels = require('@starbase/channels');
+const theRules = require('@starbase/therules');
+const Socket = require('@starbase/socket');
+const Encryption = require('@starbase/encryption');
+
+function Starbase() {
+
+  let starbase = {};
+
+  starbase.Defacto = Defacto;
+  starbase.Database = Database;
+  starbase.memstore = memstore;
+  starbase.Channels = Channels;
+  starbase.theRules = theRules;
+  starbase.Socket = Socket;
+  starbase.DBService = DBService;
+
+  return starbase;
+
+}
+
+module.exports = Starbase;
