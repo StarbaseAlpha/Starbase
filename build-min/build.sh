d75e45a09d08f84500bbc0389b42059182d27de5
Mike Fuller <emailthemike@gmail.com>
2018-12-01 19:18:43 -0500

    updates for auth package fixes

--- build-min/build.sh
+++ build-min/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-wget https://cdn.jsdelivr.net/npm/@starbase/database/database.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/database/memstore.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/channels/channels.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/channels/api/channels.api.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/socket/socket.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/therules/therules.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/encryption/encryption.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/auth/auth.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/admin/admin.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/profiles/profiles.min.js
-wget https://cdn.jsdelivr.net/npm/@starbase/pwa/pwa.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/Database/master/database.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/Database/master/memstore.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/Channels/master/channels.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/Channels/master/api/channels.api.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/Socket/master/socket.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/TheRules/master/therules.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/Encryption/master/encryption.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/Auth/master/auth.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/Admin/master/admin.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/Profiles/master/profiles.min.js
+wget https://raw.githubusercontent.com/StarbaseAlpha/PWA/master/pwa.js
 
 cat STARTjs database.min.js memstore.min.js channels.min.js channels.api.min.js socket.min.js therules.min.js encryption.min.js auth.min.js admin.min.js profiles.min.js pwa.js ENDjs > starbase.js
 
