18b0d40bf03b76f54e2e3a73f583eb9c00c45dbe
Mike Fuller <emailthemike@gmail.com>
2018-12-07 20:44:17 -0500

    build.sh is smaller still

--- build-min/build.sh
+++ build-min/build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/StarbaseAlpha/Database/master/database.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/Database/master/memstore.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/Channels/master/channels.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/Channels/master/api/channels.api.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/Socket/master/socket.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/TheRules/master/therules.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/Encryption/master/encryption.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/Auth/master/auth.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/Admin/master/admin.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/Profiles/master/profiles.min.js
-wget https://raw.githubusercontent.com/StarbaseAlpha/PWA/master/pwa.js
+REPOS_SRC="https://raw.githubusercontent.com/StarbaseAlpha/"
+wget $REPOS_SRC\Database/master/database.min.js $REPOS_SRC\Database/master/memstore.min.js $REPOS_SRC\Channels/master/channels.min.js wget $REPOS_SRC\Channels/master/api/channels.api.min.js
+wget $REPOS_SRC\Socket/master/socket.min.js $REPOS_SRC\TheRules/master/therules.min.js $REPOS_SRC\Encryption/master/encryption.min.js $REPOS_SRC\Auth/master/auth.min.js
+wget $REPOS_SRC\Admin/master/admin.min.js $REPOS_SRC\Profiles/master/profiles.min.js $REPOS_SRC\PWA/master/pwa.js
 cat STARTjs database.min.js memstore.min.js channels.min.js channels.api.min.js socket.min.js therules.min.js encryption.min.js auth.min.js admin.min.js profiles.min.js pwa.js ENDjs > starbase.js
 uglifyjs -c -o ../starbase.min.js starbase.js
 rm starbase.js database.min.js memstore.min.js channels.min.js channels.api.min.js socket.min.js therules.min.js encryption.min.js auth.min.js admin.min.js profiles.min.js pwa.js
