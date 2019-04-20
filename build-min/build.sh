73e9bca5f8a001d8633fdefde180f60f8b3a41c6
Mike Fuller <emailthemike@gmail.com>
2019-04-20 01:20:39 -0400

    removed encryption and pwa... cleanup.

--- build-min/build.sh
+++ build-min/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 REPOS_SRC="https://raw.githubusercontent.com/StarbaseAlpha/"
-wget $REPOS_SRC\Database/master/database.min.js $REPOS_SRC\Memstore/master/memstore.min.js $REPOS_SRC\Channels/master/channels.min.js wget $REPOS_SRC\Client/master/client.min.js
+wget $REPOS_SRC\Database/master/database.min.js $REPOS_SRC\Memstore/master/memstore.min.js
+wget $REPOS_SRC\Channels/master/channels.min.js $REPOS_SRC\Client/master/client.min.js
 wget $REPOS_SRC\Socket/master/socket.min.js $REPOS_SRC\TheRules/master/therules.min.js $REPOS_SRC\Auth/master/auth.min.js
 wget $REPOS_SRC\Admin/master/admin.min.js $REPOS_SRC\Profiles/master/profiles.min.js $REPOS_SRC\Services/master/services.min.js
 cat STARTjs database.min.js memstore.min.js channels.min.js client.min.js socket.min.js therules.min.js auth.min.js admin.min.js profiles.min.js services.min.js ENDjs > starbase.js
