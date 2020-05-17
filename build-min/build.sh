54b613e8b5aa5c64024a1f3673eebf0d2aa256e5
Mike Fuller <emailthemike@gmail.com>
2020-05-16 21:04:12 -0400

    added realtime and webrtc

--- build-min/build.sh
+++ build-min/build.sh
@@ -6,7 +6,7 @@ wget $REPOS_SRC\Socket/master/socket.min.js $REPOS_SRC\TheRules/master/therules.
 wget $REPOS_SRC\Admin/master/admin.min.js wget $REPOS_SRC\Profiles/master/profiles.min.js wget $REPOS_SRC\Encryption/master/encryption.min.js
 wget $REPOS_SRC\Cryptic/master/cryptic.min.js $REPOS_SRC\CryptoDB/master/cryptodb.min.js $REPOS_SRC\Topics/master/topics.min.js
 wget $REPOS_SRC\Push/master/push.min.js $REPOS_SRC\Files/master/files.min.js
-
-cat STARTjs database.min.js memstore.min.js channels.min.js client.min.js socket.min.js therules.min.js auth.min.js admin.min.js profiles.min.js pwa.min.js cryptic.min.js cryptodb.min.js encryption.min.js services.min.js topics.min.js push.min.js files.min.js ENDjs > starbase.js
+wget $REPOS_SRC\WEBRTC/master/webrtc.min.js $REPOS_SRC\Realtime/master/realtime.min.js
+cat STARTjs database.min.js memstore.min.js channels.min.js client.min.js socket.min.js therules.min.js auth.min.js admin.min.js profiles.min.js pwa.min.js cryptic.min.js cryptodb.min.js encryption.min.js services.min.js topics.min.js push.min.js files.min.js webrtc.min.js realtime.min.js ENDjs > starbase.js
 uglifyjs -c -o ../starbase.min.js starbase.js
-rm starbase.js database.min.js memstore.min.js channels.min.js client.min.js socket.min.js therules.min.js auth.min.js admin.min.js profiles.min.js pwa.min.js cryptic.min.js cryptodb.min.js encryption.min.js services.min.js topics.min.js push.min.js files.min.js
+rm starbase.js database.min.js memstore.min.js channels.min.js client.min.js socket.min.js therules.min.js auth.min.js admin.min.js profiles.min.js pwa.min.js cryptic.min.js cryptodb.min.js encryption.min.js services.min.js topics.min.js push.min.js files.min.js webrtc.min.js realtime.min.js
